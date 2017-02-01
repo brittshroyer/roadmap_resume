95c233dc588c66387f7b040ca8042c975f9cc5e8
brittshroyer <britt.shroyer@gmail.com>
2017-02-01 14:02:38 -0600

    +i90 sign

--- main.js
+++ main.js
@@ -7,12 +7,18 @@ $(document).ready(function() {
 
   var calcMileage = function(){
     var distanceRemaining = $('.states').scrollTop();
-    // console.log('distance remaining', distanceRemaining);
     var distanceTraveled = initialDistance - distanceRemaining;
     console.log('distance traveled:',distanceTraveled);
-    // $('.ohioWelcome').css('animation', 'translateOhio 2s');
+    // translate ohio sign
     $('.ohioWelcome').css('transform', 'translateZ('+distanceTraveled/6+'px)');
     $('.post').css('height', 10+distanceTraveled/16+'px');
+    if(distanceTraveled>1500){
+      var i90Distance = distanceTraveled - 1500;
+      $('.i90').css('transform', 'translateZ('+i90Distance/6+'px)');
+      $('.i90post').css('height', i90distance+'px');
+    }
+
+
   }
 
   $('.states').scroll(function(){
